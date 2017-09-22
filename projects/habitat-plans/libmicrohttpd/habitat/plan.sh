e3e350c3d1f7cc0ca0d2beb58f40133aa0135cb5
Greg Fodor <gfodor@gmail.com>
2017-09-21 21:24:02 -0700

    Added plans for libmicrohttpd and usrsctp

---
+++ projects/habitat-plans/libmicrohttpd/habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=libmicrohttpd
+pkg_origin=mozillareality
+pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
+pkg_version="0.9.55"
+pkg_source="http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.55.tar.gz"
+pkg_shasum="0c1cab8dc9f2588bd3076a28f77a7f8de9560cbf2d80e53f9a8696ada80ed0f8"
+pkg_build_deps=(core/make core/gcc) 
+
---
+++ projects/habitat-plans/usrsctp/habitat/plan.sh
@@ -0,0 +1,31 @@
+pkg_name=usrsctp
+pkg_origin=mozillareality
+pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
+pkg_version="9.4.0"
+pkg_source="https://github.com/gfodor/usrsctp/archive/0.9.4.0.tar.gz"
+pkg_shasum="7077e275125ef98d33c8bf2d88d457a806b7c5e7811c1f614d60bbca0723f69c"
+pkg_build_deps=(
+  core/make
+  core/gcc
+  core/which
+  core/libtool
+  core/m4
+  core/automake
+  core/autoconf
+)
+
+pkg_lib_dirs=(lib)
+pkg_include=(include)
+
+do_build() {
+  cd ../usrsctp-0.9.4.0
+  ./bootstrap
+  ./configure 
+  make
+}
+
+do_install() {
+  cp ../usrsctp-0.9.4.0/usrsctplib/usrsctp.h ${pkg_prefix}/include
+  cp ../usrsctp-0.9.4.0/usrsctplib/.libs/libusrsctp.a ${pkg_prefix}/lib
+  cp ../usrsctp-0.9.4.0/usrsctplib/.libs/libusrsctp.so ${pkg_prefix}/lib
+}
