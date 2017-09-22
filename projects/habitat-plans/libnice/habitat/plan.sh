d4843313f6e5ea6f79b0f28dbe72de892ab18cf0
Greg Fodor <gfodor@gmail.com>
2017-09-22 01:00:01 -0700

    Add a bunch of plans needed for libnice

--- projects/habitat-plans/libnice/habitat/plan.sh
+++ projects/habitat-plans/libnice/habitat/plan.sh
@@ -4,27 +4,9 @@ pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
 
 pkg_version="0.1.14"
 pkg_license=('MPL')
-pkg_source="https://github.com/libnice/libnice/archive/0.1.14.tar.gz"
+pkg_source="https://nice.freedesktop.org/releases/libnice-${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="28a2ccf03ac7f59fe10094e711ef777659db3c90c04d06c85b23b232626b7902"
-pkg_build_deps=(
-  core/make
-  core/gcc
-  core/which
-  core/libtool
-  core/m4
-  core/automake
-  core/autoconf
-  core/pkg-config
-  mozillareality/autoconf-archive
-)
+pkg_shasum="be120ba95d4490436f0da077ffa8f767bf727b82decf2bf499e39becc027809c"
+pkg_build_deps=(core/make core/gcc core/pkg-config mozillareality/gnutls)
 pkg_description="Libnice is an implementation of the IETF's Interactive Connectivity Establishment (ICE) standard (RFC 5245)"
 pkg_upstream_url="https://nice.freedesktop.org/wiki/"
-
-do_build() {
-  PKG_NAME=libnice
-  aclocal --install
-  autoreconf --force --install
-  ./configure --prefix=${pkg_prefix}
-  make
-}
